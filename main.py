d64e70bd23ff6bf3ca1d36aba80260be053b822c
Kunal Gaikwad <gaikwadkunal610@gmail.com>
2024-01-17 17:44:12 +0000

    195. Detect Collisions with your own Tail

--- main.py
+++ main.py
@@ -26,9 +26,18 @@ while game_is_on:
     # Collision detection with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend_snake()
         scoreboard.increase_score()
     # Collision detection with wall
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() >= 295 or snake.head.ycor() <= -295:
+    if abs(snake.head.xcor()) > 280 or snake.head.ycor() > 280:
         game_is_on = False
         scoreboard.game_over()
+    # Collision detection with tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -17,13 +17,19 @@ class Snake:
 
     def create_snake(self):
         for segment in range(0, 3):
-            new_segment = Turtle()
-            new_segment.speed("fastest")
-            new_segment.penup()
-            new_segment.shape("square")
-            new_segment.goto(STARTING_POSITION[segment])
-            new_segment.color("white")
-            self.segments.append(new_segment)
+            self.add_segment(STARTING_POSITION[segment])
+
+    def add_segment(self, position):
+        new_segment = Turtle()
+        new_segment.speed("fastest")
+        new_segment.penup()
+        new_segment.shape("square")
+        new_segment.goto(position)
+        new_segment.color("white")
+        self.segments.append(new_segment)
+
+    def extend_snake(self):
+        self.add_segment(self.segments[-1].pos())
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
